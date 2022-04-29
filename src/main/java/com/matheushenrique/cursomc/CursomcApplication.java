e2cad92f781078f4a0db776a5e22e307f0fc84a0
Matheus Henrique <m_peu@hotmail.com>
2022-04-29 20:11:51 -0300

    Projeto criado

---
+++ src/main/java/com/matheushenrique/cursomc/CursomcApplication.java
@@ -0,0 +1,13 @@
+package com.matheushenrique.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+}
