b5ea87b1a1042ff10d3365e901da01a90b49c5e5
Matheus Henrique <m_peu@hotmail.com>
2022-05-02 21:13:12 -0300

    Criando operacao de instaciacao

--- src/main/java/com/matheushenrique/cursomc/CursomcApplication.java
+++ src/main/java/com/matheushenrique/cursomc/CursomcApplication.java
@@ -1,13 +1,32 @@
 package com.matheushenrique.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.matheushenrique.cursomc.dao.CategoriaDAO;
+import com.matheushenrique.cursomc.domain.Categoria;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaDAO categoriaDAO;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaDAO.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
--- src/main/java/com/matheushenrique/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/matheushenrique/cursomc/resources/CategoriaResource.java
@@ -1,8 +1,5 @@
 package com.matheushenrique.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,9 +19,7 @@ public class CategoriaResource {
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
-		
 		Categoria obj = categoriaService.find(id);
-		
 		return ResponseEntity.ok().body(obj);
 	}
 
